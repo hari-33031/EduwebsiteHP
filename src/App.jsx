06cebfda802eacd64032d2ab3a6f44bb8af1ec99
hari-33031 <harisrinivasarao2006@gmail.com>
2026-02-16 18:38:10 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <About setPlayState={setPlayState}/>
         <Title subTitle='Gallery' title='Campus Photos'/>
         <Campus/>
-        <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+        <Title subTitle='TESTIMONIALS' title='What Student Says accordingly hegegege'/>
         <Testimonials/>
         <Title subTitle='Contact Us' title='Get in Touch'/>
         <Contact/>
